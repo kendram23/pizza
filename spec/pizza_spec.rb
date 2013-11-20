f58a5d2b8f7137989aa5d4b168fc99f75afbde30
Kendra McClanahan <kendramcclanahan@kendras-mbp.att.net>
2013-11-20 15:51:39 -0600

    Created a failing test for setting a default topping for the pizza.

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -12,7 +12,14 @@ describe Pizza::Pie do
 		]
 		pizza = Pizza::Pie.new(toppings)
 
-		expect(pizza.toppings).new(toppings)
+		expect(pizza.toppings).to eq(toppings)
+		end
+
+		it 'defaults the toppings to cheese only, if the pizza has no toppings' do
+		pizza = Pizza::Pie.new
+
+		expect(pizza.toppings.size).to eq(1)
+		expect(pizza.toppings.first.name).to eq('cheese')
 		end
 	end
 end 
